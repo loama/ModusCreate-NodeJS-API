23c9fbd526397e84949daa5fd3ed80d731ce79c8
Jaime Lopez <loama18@gmail.com>
2018-11-04 21:16:09 -0600

    add 404 function

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const PORT = process.env.PORT || 8888
 
 express()
   .get('/', (req, res) => res.send(appointments(req)))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('*', (req, res) => res.status(404).send(notFound(req)))
+  .listen(PORT, () => console.log(`Listening on PORT: ${ PORT }, open http://localhost:${ PORT } to preview it`))
 
 function appointments (req) {
-  console.log(req)
-  return req.path
+  // console.log(req)
+  return {path: req.path}
+}
+
+function notFound(req) {
+  return {result: 'error', detail: 'route not found'}
 }
