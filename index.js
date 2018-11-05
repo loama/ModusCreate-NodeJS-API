74a468a56b6bb3469b01aeff061ef34b4bab971e
Jaime Lopez <loama18@gmail.com>
2018-11-04 21:08:26 -0600

    add readme and instructions

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const express = require('express')
 const PORT = process.env.PORT || 8888
 
 express()
-  .get('/', (req, res) => res.send('pages/index'))
+  .get('/', (req, res) => res.send(appointments(req)))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function appointments (req) {
+  console.log(req)
+  return req.path
+}
