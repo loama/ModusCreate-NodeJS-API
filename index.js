537825e11373b7635a27c2d7b6e52e2ffb66ee91
Jaime Lopez <loama18@gmail.com>
2018-11-04 23:06:29 -0600

    add post functionality

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const PORT = process.env.PORT || 8888
 const request = require('request')
 
 express()
-  .get('/', (req, res) => appointments(req,res))
+  .use(express.json())
+  .get('/', (req, res) => index(req,res))
   .get('/vehicles/*', (req, res) => vehicles(req, res))
+  .post('/vehicles', (req, res) => vehicles(req, res))
   .get('*', (req, res) => res.status(404).send(notFound(req)))
   .listen(PORT, () => console.log(`Listening on PORT: ${ PORT }, open http://localhost:${ PORT } to preview it`))
 
 const api = 'https://one.nhtsa.gov/webapi/api/'
 
-function appointments (req, res) {
+function index (req, res) {
   // console.log(req)
   res.send({result: 'success', detail: 'welcome to NHTSA Safety API'})
 }
@@ -20,9 +22,18 @@ function error (message) {
 }
 
 function vehicles (req, res) {
-  const path = req.path.split('/')
+  let modelYear, make, model
+  if (req.method === 'GET') {
+    const path = req.path.split('/')
+    modelyear = path[2]
+    make = path[3]
+    model = path[4]
+  } else if (req.method === 'POST') {
+    modelyear = req.body.modelYear
+    make = req.body.manufacturer
+    model = req.body.model
+  }
 
-  let modelyear = path[2]
   // check that model is a valid year
   if (1900 < modelyear && modelyear < 2020) {
     // valid
@@ -30,15 +41,13 @@ function vehicles (req, res) {
     res.status(400).send(error('invalid model year specified'))
   }
 
-  let make = path[3]
   // check that make is a valid string
   if (0 < make.length && make.length < 20) {
     // valid
   } else {
-    res.status(400).send(error('invalid make specified'))
+    res.status(400).send(error('invalid make (manufacturer) specified'))
   }
 
-  let model = path[4]
   // check that model is a valid string
   if (0 < model.length && model.length < 20) {
     // valid
